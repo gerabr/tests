ffd5685fa7be17e47383e11c85466e49618bc3f1
Herman Brigadir <hermanb@hermanbs-Air.lan>
2022-10-02 20:15:42 +0300

    Show user before printing

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ import os
 def list_dir(directory='.'):
 	return os.listdir(directory)
 
+def show_user():
+	os.system('id')
+
 def main():
+	show_user()
 	a = list_dir()
 	for dir in a:
 		print(f"Filename: {dir}")
